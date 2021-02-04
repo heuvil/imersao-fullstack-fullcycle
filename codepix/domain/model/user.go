527aff57f4fe246dec0e58585a3fa9c007e1426d
heuvil <heuler@outlook.com>
2021-02-03 21:48:45 -0300

    criação da entidade User

---
+++ codepix/domain/model/user.go
@@ -0,0 +1,43 @@
+//ID (uuid), Name e Email
+//implemente uma função NewUser que seja capaz de validar que ID, Name e Email são obrigatórios, caso o contrário, ela deve retornar um erro.
+
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
+
+type User struct {
+	Base     `valid:"required"`
+	Name     string     `json:"name" gorm:"type:varchar(255)" valid:"notnull"`
+	Email    string     `json:"email" gorm:"type:varchar(255)" valid:"notnull"`
+}
+
+func (user *User) isValid() error {
+	_, err := govalidator.ValidateStruct(user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewUser(name string, email string) (*User, error) {
+	user := User{
+		Name: name,
+		Email: email,
+	}
+	user.ID = uuid.NewV4().String()
+	user.CreatedAt = time.Now()
+	err := user.isValid()
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
